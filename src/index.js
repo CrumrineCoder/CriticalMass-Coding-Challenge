361ed06e8a7f2414e020553505b9b1522740fc06
CrumrineCoder <crumrinecoding@gmail.com>
2019-05-08 18:32:56 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+const song = `Baby Shark doo doo doo doo doo doo
+Baby Shark doo doo doo doo doo doo
+Baby Shark doo doo doo doo doo doo
+Baby Shark!
+Daddy Shark doo doo doo doo doo doo
+Daddy Shark doo doo doo doo doo doo
+Daddy Shark doo doo doo doo doo doo
+Daddy Shark!
+Mommy Shark doo doo doo doo doo doo
+Mommy Shark doo doo doo doo doo doo
+Mommy Shark doo doo doo doo doo doo
+Mommy Shark!
+Grandpa Shark doo doo doo doo doo doo
+Grandpa Shark doo doo doo doo doo doo
+Grandpa Shark doo doo doo doo doo doo
+Grandpa Shark!
+Grandma Shark doo doo doo doo doo doo
+Grandma Shark doo doo doo doo doo doo
+Grandma Shark doo doo doo doo doo doo
+Grandma Shark!`;
+
+// Split the song String into an array by new line
+
+console.log(song.split("\n"));
