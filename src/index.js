0e4fd95995d79eb2d3a893c150b5be64f207c368
CrumrineCoder <crumrinecoding@gmail.com>
2019-05-09 12:03:59 -0400

    1.5 second iteration added
    Moved from for loop to recursion, with two different methods. The first uses an iterator variable to count up through the array, while the second removes the first element from the array each time and only selects the first one each iteration.

--- src/index.js
+++ src/index.js
@@ -22,10 +22,56 @@ Grandma Shark!`;
 // Split the song String into an array by new line
 
 var splitSong = song.split("\n");
-for(var i=0; i<splitSong.length; i++){
-    var node = document.createElement("LI");  
-    var textnode = document.createTextNode(splitSong[i])
-    node.appendChild(textnode);
-    document.getElementById("lyrics-list").appendChild(node);
+
+// Function will loop through the song array of strings generating <li> elements inside of <ul> until iteration variable reaches end of array
+function displaySongLine(arrayOfLyrics, iterator) {
+    setTimeout(function () {
+        
+        // Create <li> element with one line of text from song
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(arrayOfLyrics[iterator])
+        node.appendChild(textnode);
+        
+        // Add <li> element to <ul>
+        document.getElementById("lyrics-list").appendChild(node);
+
+        //Iterate by 1
+        iterator++;
+
+        // If we're not done with the song, continue regression
+        if (iterator < arrayOfLyrics.length) {
+            displaySongLine(arrayOfLyrics, iterator);
+        }
+    // Delay each loop by 1.5 seconds
+    }, 1500)
 }
 
+// Start the loop
+displaySongLine(splitSong, 0);
+
+/*
+// Alternative function that uses recursion more heavily and relies less on an iteration variable
+// Function will loop through the song array of strings generating <li> elements inside of <ul> until it runs out of array
+function displaySongLineAlt(arrayOfLyrics) {
+    setTimeout(function () {
+        
+        // Create <li> element with one line of text from song
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(arrayOfLyrics[0])
+        node.appendChild(textnode);
+        
+        // Add <li> element to <ul>
+        document.getElementById("lyrics-list").appendChild(node);
+
+        arrayOfLyrics.shift();
+        // If we're not done with the song, continue regression
+        if (arrayOfLyrics.length) {
+            displaySongLineAlt(arrayOfLyrics);
+        }
+    // Delay each loop by 1.5 seconds
+    }, 1500)
+}
+
+// Start the loop
+displaySongLineAlt(splitSong);
+*/
